394a96ffce2fef3d3cd44bc68f0027fa0d9523df
Jacek Kuźmicz <snwcore@gmail.com>
2021-10-30 17:22:43 +0200

    serializer / deserializer #3

--- Classes/Business/ResponseObject.cs
+++ Classes/Business/ResponseObject.cs
@@ -9,15 +9,21 @@ namespace zlinz_3_k26.Classes.Business
     public class ResponseObject
     {
         [DataMember]
-        public int ResponseCode { get; set; }
+        public int ResponseCode { get; private set; }
 
         [DataMember]
-        public string ResponseString { get; set; }
+        public string ResponseString { get; private set; }
 
         public ResponseObject()
         {
             ResponseCode = -1;
             ResponseString = "Brak odpowiedzi";
+        }
+
+        public ResponseObject(int ResponseCode, string ResponseString)
+        {
+            this.ResponseCode = ResponseCode;
+            this.ResponseString = ResponseString;
         }
 
         public override string ToString()
