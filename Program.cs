aa262ae3a62f9391fed72bb0361a569b943a2035
Jacek Kuźmicz <snwcore@gmail.com>
2021-10-30 15:10:29 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zlinz_3_k26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
